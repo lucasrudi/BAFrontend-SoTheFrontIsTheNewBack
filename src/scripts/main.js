27f2ed622060435a2da3991028a840aca61e1ead
Lucas Rudi <rudilucas@gmaill.com>
2015-05-26 16:01:27 -0300

    initial commit ?

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,18 +6,30 @@ var bespoke = require('bespoke'),
   bullets = require('bespoke-bullets'),
   backdrop = require('bespoke-backdrop'),
   scale = require('bespoke-scale'),
+  state = require('bespoke-state'),
   hash = require('bespoke-hash'),
+  nebula = require('bespoke-theme-nebula'),
+  camera = require('bespoke-camera'),
+  click = require('bespoke-click'),
+  notes = require('bespoke-notes'),
+  ignite = require('./ignite'),
+  //sync = require('bespoke-sync'),
   progress = require('bespoke-progress');
 
 // Bespoke.js
 bespoke.from('article', [
-  cube(),
+  nebula(),
+  camera(),
+  click(),
+  notes(),
   keys(),
+  state(),
   touch(),
   bullets('li, .bullet'),
   backdrop(),
   scale(),
   hash(),
+  ignite(),
   progress()
 ]);
 
@@ -25,4 +37,3 @@ bespoke.from('article', [
 // This is actually loaded from "bower_components" thanks to
 // debowerify: https://github.com/eugeneware/debowerify
 require('prism');
-
